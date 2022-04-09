14440027645e5b4b956552402c6b36f96b14dca2
klimak25 <klim25mi@gmail.com>
2022-04-09 17:44:54 +0300

    env

--- src/layuut/Main.js
+++ src/layuut/Main.js
@@ -3,7 +3,7 @@ import {Movies} from '../components/Movies'
 import {Preloader} from '../components/Preloader'
 import { Search } from "../components/Search"
  
-const API_KEY = process.env.REACT_APP_API_KEY
+const API_KEY = '8fb70000'
   class Main extends React.Component {
 
     state = {
